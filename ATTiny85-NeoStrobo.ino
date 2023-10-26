3223ac26c37fbaae5551511f205a767e1c463758
John Rogers <k1wiz@wizworks.net>
2023-10-25 22:21:03 -0400

    Create ATTiny85-NeoStrobo.ino

---
+++ ATTiny85-NeoStrobo.ino
@@ -0,0 +1,57 @@
+/*
+ * NeoPixel Ring Color Strobe
+ * 
+ * written by: John Rogers     john at wizworks dot net
+ * revision 1   Oct 25 2023
+ * 
+ * License: GPL
+ * Free for all use both commercial and private so long as this notice persists in all derivatives.
+ */
+
+#include <Adafruit_NeoPixel.h>
+
+#define PIN            2  // Pin connected to the NeoPixels
+#define NUMPIXELS      16  // Number of NeoPixels in the ring
+#define ADJPIXELS      1  // Number of adjacent pixes to fire
+
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
+
+// Define an array of specified colors, including white
+uint32_t rainbowColors[] = {
+  strip.Color(255, 255, 255),  // White
+  strip.Color(255, 0, 0),     // Red
+  strip.Color(255, 127, 0),   // Orange
+  strip.Color(255, 255, 0),   // Yellow
+  strip.Color(0, 255, 0),     // Green
+  strip.Color(0, 0, 255),     // Blue
+  strip.Color(127, 0, 255)    // Violet
+};
+
+void setup() {
+  strip.begin();
+  strip.show();  // Initialize all pixels to 'off'
+  randomSeed(analogRead(0));  // Seed the random number generator
+}
+
+void loop() {
+  int pixel = random(NUMPIXELS - 3);  // Randomly choose a pixel (avoid last three pixels)
+
+  // Randomly choose a color from the specified set
+  uint32_t color = rainbowColors[random(7)];
+
+  // Set the adjacent four pixels to the random color
+  for (int i = 0; i < ADJPIXELS; i++) {
+    strip.setPixelColor(pixel + i, color);
+  }
+  strip.show();
+
+  delay(50);  // Keep the pixels on for 0.5 seconds
+
+  // Turn off the pixels
+  for (int i = 0; i < ADJPIXELS; i++) {
+    strip.setPixelColor(pixel + i, 0);
+  }
+  strip.show();
+
+  delay(random(50, 100));  // Random off time between bursts (1-3 seconds)
+}
